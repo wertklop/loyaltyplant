b6e57c48004073f13493db4ecdef609a8846eccd
Gennadii K <wertklop@yandex.ru>
2015-04-17 08:49:27 +0300

    Добавление первого теста

--- src/test/java/com/loyaltyplant/LoyaltyplantApplicationTests.java
+++ src/test/java/com/loyaltyplant/LoyaltyplantApplicationTests.java
@@ -1,12 +1,45 @@
 package com.loyaltyplant;
 
 import com.loyaltyplant.config.LoyaltyplantApplicationConfiguration;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.sql.DataSource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration(classes = LoyaltyplantApplicationConfiguration.class)
 public class LoyaltyplantApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void mainPageTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
 }
